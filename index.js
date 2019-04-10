91c305969d0b2bd118e2beb19e011895dc49f01d
Teon L Brooks <teon.brooks@gmail.com>
2019-04-10 11:12:05 -0400

    upgrade to using openbci-observable

--- index.js
+++ index.js
@@ -2,34 +2,21 @@ require('./text-polyfill');
 const { Observable } = require('rxjs');
 
 const lsl = require('node-lsl');
-const Cyton = require("@openbci/cyton");
+const { Cyton } = require("openbci-observable");
 
 
 async function connect() {
     let client = await new Cyton({ verbose: true });
+    await client.connect();
+    await client.start();
 
-    client.autoFindOpenBCIBoard().then(portName => {
-      if (portName) {
-        client.connect(portName)
-              .then(portName => {
-                console.log(`Connected. device: ${portName}`);
-                client.on("ready", () => {
-                client.streamStart();
-              })
-            )
-            return client;
-      } else {
-        console.log("Device not found.")
-      }}
-    })
-  }
+    return client;
+}
 
 function streamLsl(client) {
     console.log('LSL: Creating Stream...');
 
-    // These packets keep the connection alive
-    const keepaliveTimer = setInterval(() => client.sendCommand(''), 3000);
-    const info = device.getInfo();
+    const info = client.getInfo();
     const n_channels = info.numberOfChannels;
     const sfreq = info.sampleRate;
     const name = info.boardType
@@ -48,7 +35,7 @@ function streamLsl(client) {
     const outlet = lsl.create_outlet(info, 0, 360);
     let sampleCounter = 0;
 
-    Observable.from(client.on('sample'))
+    Observable.from(client)
         .finally(() => {
             lsl.lsl_destroy_outlet(outlet);
             clearInterval(keepaliveTimer);
@@ -62,9 +49,3 @@ function streamLsl(client) {
             process.stdout.write(`LSL: Sent ${sampleCounter} samples`);;
         });
 }
-
-noble.on('stateChange', (state) => {
-    if (state === 'poweredOn') {
-        connect().then(streamLsl);
-    }
-});
